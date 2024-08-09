aca95dc1aaee2d4c9b609766bfc4efad02909895
german.munshau <g.munshau@yandex.ru>
2024-08-09 15:59:00 +0600

    refactoring

--- src/components/AgreementHistoryItem/AgreementHistoryItem.jsx
+++ src/components/AgreementHistoryItem/AgreementHistoryItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {getDate} from "../../utils/utils";
 import './agreementhistory-item.css'
 
-
 const AgreementHistoryItem = ({item}) => {
     return (
         <tr className={'document-item'}>
@@ -13,13 +12,6 @@ const AgreementHistoryItem = ({item}) => {
                 <div><b>Комментарий: </b>{item.comment}</div>
             </td>
         </tr>
-        // <div className={'document-item'}>
-        //     <div><b>Дата: </b> {item.date && getDate(item.date)}</div>
-        //     <div><b>Чья Виза: </b>{item?.author?.name}</div>
-        //     <div><b>Решение: </b>{item.agreed}</div>
-        //     <div><b>Комментарий: </b>{item.comment}</div>
-        //     <hr/>
-        // </div>
     );
 };
 
